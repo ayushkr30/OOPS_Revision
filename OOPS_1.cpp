91a385f6189af6e66af6bb087803f21d1781b44b
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-07-02 20:39:04 +0530

    Update OOPS_1.cpp

--- OOPS_1.cpp
+++ OOPS_1.cpp
@@ -43,9 +43,6 @@ int main() {
   carObj2.model = "Mustang";
   carObj2.year = 1969;
 
-  // Print attribute values
-  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
-  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
-  return 0;
+  
 }
 
